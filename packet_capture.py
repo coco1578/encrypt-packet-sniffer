638b3578c594c18874ae997382464173cc80e1e4
coco1578 <rlawnsgh0826@gmail.com>
2020-12-15 17:37:51 +0900

    Add Packet Capture Process Class - (2020.12.15 support only Dumpcap)

---
+++ packet_capture.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+
+from log import logger
+
+
+class Capture:
+
+    def __init__(self, config):
+
+        assert config is not None
+
+        self.config = config
+
+        self.packet_capture_process = None # subprocess.Popen()
+        self.packet_capture_program = None
+        self.packet_capture_filter = None
+        self.save_path = None
+        self.duration = None
+        self.file_size = None
+        self.network_adaptor = None
+        self.command = None
+
+        self.process = None
+
+        self._init_capture_setup()
+        self._init_capture_program()
+
+    def _init_capture_setup(self):
+
+        self.packet_capture_program = self.config['CaptureProgram']['program']
+        self.packet_capture_filter = self.config['CaptureProgram']['filter']
+        self.save_path = self.config['CaptureProgram']['save_path']
+        self.duration = self.config['CaptureProgram']['duration']
+        self.file_size = self.config['CaptureProgram']['file_size']
+        self.network_adaptor = self.config['CaptureProgram']['adaptor']
+
+    def _init_capture_program(self):
+
+        # Will be support tcpdump & tshark
+        self.command = '{} -a duration:{} -a filesize:{} -i {} -s 0 -f \'{}\' -w {}'.format(self.packet_capture_program, self.duration, self.file_size, self.network_adaptor, self.packet_capture_filter, self.save_path) # dumpcap
+        logger.info('Packet Capture command %s' % self.command)
+
+    def start(self):
+
+        try:
+            self.process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            logger.info('Dumpcap process start')
+        except subprocess.SubprocessError:
+            self.stop()
+            logger.error('Subprocess error')
+        except subprocess.CalledProcessError:
+            self.stop()
+            logger.error('Call dumpcap error')
+
+    def stop(self):
+
+        self.process.kill()
+        logger.info('Dumpcap process stop')
+
+    def remove(self):
+
+        remove_command = 'rm -rf %s' % self.save_path
+        remove_process = subprocess.Popen(remove_command, stdout=subprocess.PIPE)
